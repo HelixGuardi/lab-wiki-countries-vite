3373f96f460c5300325eb25417b6dd8568692c2e
Victor Hugo Guardiola <victorhugo.pessoal.guardiola@gmail.com>
2024-12-27 11:51:35 +0100

    fixed styles and structure

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,11 @@
 function Navbar() {
-
-    return(
-        <nav class="navbar navbar-dark bg-primary mb-3">
-        <div class="container">
-          <a class="navbar-brand" href="/">WikiCountries</a>
-        </div>
-      </nav>
-    )
+  return (
+    <nav className="navbar navbar-dark bg-primary mb-3">
+      <div className="container">
+        <a className="navbar-brand" href="/">WikiCountries</a>
+      </div>
+    </nav>
+  );
 }
 
 export default Navbar;
--- src/pages/CountryDetailsPage.jsx
+++ src/pages/CountryDetailsPage.jsx
@@ -1,7 +1,37 @@
 function CountryDetails() {
 
     return(
-        <h3>Country Details</h3>
+        <div className="container">
+            <p style={{ fontSize: "24px", fontWeight: "bold" }}>Country Details</p>
+
+            <h1>Country Name*</h1>
+
+            <table className="table">
+                <thead></thead>
+                <tbody>
+                    <tr>
+                        <td style={{ width: "30%" }}>Capital</td>
+                        <td>Capital Name*</td>
+                    </tr>
+                    <tr>
+                        <td>Area</td>
+                        <td>
+                            Area Extension*
+                            <sup>2</sup>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Borders</td>
+                        <td>
+                            <ul>
+                                <li><a href="/country-border*"></a>country border*</li>
+                                {/* ... */}
+                            </ul>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
     )
 }
 
